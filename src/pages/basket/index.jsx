73daf8c3454777b52eb72a43d79ef9714eb804c5
Ramal Abbassov <ramalabbasov044@gmail.com>
2023-11-10 22:57:43 +0400

    add

--- src/pages/basket/index.jsx
+++ src/pages/basket/index.jsx
@@ -1,7 +1,9 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import Header from '../../components/Static/Header/index'
 import styles from './style.module.css'
 import { useState } from 'react'
 const Basket = () => {
+  let navigate = useNavigate()
   let card_obj = localStorage.getItem('card_obj')
   var data = JSON.parse(card_obj)
   
@@ -23,7 +25,7 @@ const Basket = () => {
             <tbody>
               {
                 data?.map((item,index) => (
-                  <tr className={styles.tableTr}>
+                  <tr className={styles.tableTr} onClick={() => navigate(`/product-detail/${item.id}`)}>
                     <th>{index + 1}</th>
                     <td className={styles.CardName}><img className={styles.tableImg} src={item.img} />{item.name}</td>
                     <td className={styles.TableTd}>{item.price}</td>
